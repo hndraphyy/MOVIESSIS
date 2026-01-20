925aec9d9aa0f1e22608ec8cd1b1117de766402f
hndraphyy <hendraaditya1913@gmail.com>
2026-01-20 20:20:45 +0700

    fix: loading img gendre

--- src/components/card/GenreCard.tsx
+++ src/components/card/GenreCard.tsx
@@ -6,18 +6,25 @@ interface GenreCardProps {
   title: string;
   imageUrl: string;
   onClick?: () => void;
+  isPriority?: boolean;
 }
 
-const GenreCard: React.FC<GenreCardProps> = ({ title, imageUrl, onClick }) => {
+const GenreCard: React.FC<GenreCardProps> = ({
+  title,
+  imageUrl,
+  onClick,
+  isPriority,
+}) => {
   return (
     <div className="w-full px-1 py-2" onClick={onClick}>
       <Image
         src={imageUrl}
         alt={title}
-        width={800}
-        height={1000}
+        width={300}
+        height={450}
         className="w-full rounded-lg  transform transition-transform duration-500 hover:scale-[1.02] "
-        loading="lazy"
+        sizes="(max-width: 768px) 50vw, 33vw"
+        priority={isPriority}
       />
       <p className="pt-3 text-[14px] md:text-base text-gray-300 font-medium truncate w-full">
         {title}
